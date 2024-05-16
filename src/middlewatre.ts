124742581ab79e5284aa603b0294008be606977c
Nicodav28 <esteveznicolas0@gmail.com>
2024-05-16 17:59:45 -0500

    ✨ (plura-logo.svg): añadir logo de Plura en formato SVG ✨ (preview.png, appstore.png, isologo.png, stripelogo.png): añadir imágenes en formato PNG ✨ (layout.tsx, layout.tsx, layout.tsx): añadir nuevos componentes de diseño para la aplicación Se han añadido varios archivos nuevos al proyecto, incluyendo un logo en formato SVG, imágenes en formato PNG y nuevos componentes de diseño para la aplicación. Estos cambios se han realizado para mejorar la apariencia y funcionalidad de la aplicación.
    ✨ (page.tsx): Añadir una nueva página de inicio con secciones de presentación y planes de precios.
    ✨ (mode-toggle.tsx): Crear un componente de cambio de tema para alternar entre temas claro, oscuro y del sistema.
    ✨ (navigation/index.tsx): Crear un componente de navegación con enlaces a secciones y botones de inicio de sesión.
    ✨ (contants.ts): Añadir constantes para los diferentes planes de precios.
    ✨ (theme-provider.tsx): Crear un proveedor de tema para gestionar los temas en la aplicación.
    Estos cambios se realizaron para introducir una nueva página de inicio con secciones de presentación y planes de precios, así como componentes para cambiar el tema y la navegación en la aplicación. Se agregaron constantes para los planes de precios y un proveedor de tema para gestionar los temas en la aplicación.

--- src/middlewatre.ts
+++ src/middlewatre.ts
@@ -2,11 +2,13 @@ import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
 const isPublicRoute = createRouteMatcher(["/site", "/api/uploadthing"]);
 
-export default clerkMiddleware((auth, request) => {
-  if (!isPublicRoute(request)) {
+export default clerkMiddleware(
+  (auth, req) => {
+    // if (isPublicRoute(req)) return;
     auth().protect();
-  }
-});
+  },
+  { debug: true }
+);
 
 export const config = {
   // The following matcher runs middleware on all routes
