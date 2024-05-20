8268db51d957723bde956766082388d60b44d366
Nicodav28 <esteveznicolas0@gmail.com>
2024-05-20 18:23:49 -0500

    ✨ (page.tsx): añadir componente AgencyDetails y funciones de redirección 📝 (agency-detail.tsx): crear componente AgencyDetails Se ha añadido el componente AgencyDetails para mostrar los detalles de la agencia en la página principal de la agencia. Se han añadido funciones de redirección para manejar diferentes roles de usuario y redirigirlos a las páginas correspondientes. La creación del componente AgencyDetails permite mostrar los detalles de la agencia de forma estructurada en la interfaz de usuario.
    ♻️ (queries.ts): Refactorizar importaciones y eliminar código comentado
    📝 (queries.ts): Añadir comentarios y mejorar legibilidad del código
    Se refactorizaron las importaciones en el archivo queries.ts para eliminar importaciones no utilizadas y mejorar la legibilidad del código. Además, se eliminó código comentado y se añadieron comentarios para explicar la funcionalidad de las funciones y mejorar la comprensión del código.
    
    🔧 (queries.ts): Refactorizar la función saveActivityLogsNotification y crear la función auxiliar getAgencyId
    La refactorización de la función saveActivityLogsNotification permite mejorar la legibilidad y mantenibilidad del código al separar la lógica de obtención de datos del usuario en la función auxiliar getUserData. La creación de la función auxiliar getAgencyId facilita la obtención del agencyId de forma más clara y reutilizable en diferentes partes del código.
    
    ✨ (server.ts): añadir función para manejar la creación del usuario del equipo y verificación y aceptación de invitaciones
    Se ha añadido una nueva función llamada handleCreateTeamUser que se encarga de crear un usuario del equipo con los detalles proporcionados. También se ha agregado una función llamada verifyAndAcceptInvitation para manejar la verificación y aceptación de invitaciones de usuario. Estas funciones mejoran la funcionalidad del sistema al permitir la creación y aceptación de usuarios del equipo de manera más eficiente y estructurada.
    
    ♻️ (functions.ts): refactorizar las funciones getDomainContent y getPipelines
    Se eliminaron las funciones getDomainContent y getPipelines y se reemplazaron por una función más simple que devuelve el agencyId si agency existe, de lo contrario, devuelve null. Esta refactorización simplifica el código y mejora su legibilidad al eliminar código innecesario y redundante.

--- src/app/(main)/agency/page.tsx
+++ src/app/(main)/agency/page.tsx
@@ -1,15 +1,70 @@
+import AgencyDetails from "@/components/forms/agency-detail";
 import { getAuthUserDetails, verifyAndAcceptInvitation } from "@/lib/queries";
 import { currentUser } from "@clerk/nextjs";
+import { Plan } from "@prisma/client";
 import { redirect } from "next/navigation";
 import React from "react";
 
-const Page = async () => {
+const handleRedirect = (path: string) => {
+  return redirect(path);
+};
+
+const handleUserRoleRedirect = (
+  user: any,
+  agencyId: string,
+  searchParams: any
+) => {
+  switch (user?.role) {
+    case "SUBACCOUNT_GUEST":
+    case "SUBACCOUNT_USER":
+      return handleRedirect("/subaccount");
+    case "AGENCY_OWNER":
+    case "AGENCY_ADMIN":
+      if (searchParams.plan) {
+        return handleRedirect(
+          `/agency/${agencyId}/billing?plan=${searchParams.plan}`
+        );
+      }
+
+      if (searchParams.state) {
+        const [statePath, stateAgencyId] = searchParams.state.split("___");
+        if (!stateAgencyId) return <div>Not authorized</div>;
+        return handleRedirect(
+          `/agency/${stateAgencyId}/${statePath}?code=${searchParams.code}`
+        );
+      }
+
+      return handleRedirect(`/agency/${agencyId}`);
+    default:
+      return <div>Not authorized</div>;
+  }
+};
+
+const Page = async ({
+  searchParams,
+}: {
+  searchParams: { plan: Plan; state: string; code: string };
+}) => {
   const agencyId = await verifyAndAcceptInvitation();
+  console.log(agencyId);
+
   const user = await getAuthUserDetails();
 
   if (agencyId) {
+    return handleUserRoleRedirect(user, agencyId, searchParams);
   }
-  return <div>Agency Dashboard</div>;
+
+  const authUser = await currentUser();
+  return (
+    <div className="flex justify-center items-center mt-4">
+      <div className="max-w-[850px] border-[1px] p-4 rounded-xl">
+        <h1 className="text-4xl"> Create An Agency</h1>
+        <AgencyDetails
+          data={{ companyEmail: authUser?.emailAddresses[0].emailAddress }}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Page;
---
+++ src/components/forms/agency-detail.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const AgencyDetails = ({ data }: { data: any }) => {
+  return <div>AgencyDetails</div>;
+};
+
+export default AgencyDetails;
